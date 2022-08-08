f2a4585f8b7dab9936af1b669d03562649b1224f
PRS120 <109905476+PRS120@users.noreply.github.com>
2022-08-09 00:17:18 +0530

    Initial commit

---
+++ stepsToBuildBackend.js
@@ -0,0 +1,6 @@
+[
+    `Initial commit: 
+    1. Install node js
+    2. Run "npm init -y" to initialize the package.json file that tracks dependencies
+    3. Run "git init" to initialize the repository to be tracked by Git`
+]
