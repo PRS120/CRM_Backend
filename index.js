00a739e06e9fcd9f38e2a5f4942976a2402535d7
PRS120 <109905476+PRS120@users.noreply.github.com>
2022-08-10 23:05:53 +0530

    Added MongoDB, DotEnv related changes

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-const express = require('express')
+import express from "express";
+import {MongoClient} from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+console.log(process.env.MONGO_URL);
+
 const app = express()
+
+const PORT = process.env.PORT;
+const MONGO_URL = process.env.MONGO_URL;
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(4000)
+app.listen(PORT, ()=>{console.log("Server started running on port: ", PORT)});
