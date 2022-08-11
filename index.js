29edc9678b953946de7dbdc05e56cd9849942c1b
PRS120 <109905476+PRS120@users.noreply.github.com>
2022-08-12 00:25:24 +0530

    Started writing stuff for users route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {MongoClient} from "mongodb";
 import dotenv from "dotenv";
+import {usersRouter} from './routes/users.js';
 
 dotenv.config();
 console.log(process.env.MONGO_URL);
@@ -10,8 +11,21 @@ const app = express()
 const PORT = process.env.PORT;
 const MONGO_URL = process.env.MONGO_URL;
 
+async function createConnection(){
+  const client = new MongoClient(MONGO_URL);
+  await client.connect();
+  console.log("mongo is connected");
+  return client;
+}
+export const client = await createConnection();
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('CRM Backend')
 })
+
+app.use('/users', usersRouter);
+
+
 
 app.listen(PORT, ()=>{console.log("Server started running on port: ", PORT)});
